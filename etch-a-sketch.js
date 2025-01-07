6c82042634b857ec4e94dfdb02020cc85d89781e
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-06 21:26:48 -0500

    Add trail of randomized colors that darken

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,12 @@
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
 container.style.padding = (container.width / 10);
 
+const sketchArea = document.querySelector('#sketch-area');
+
+const squareClass = document.querySelector('.square');
+
 let size = 16;
+
 
 const squares = [];
 
@@ -13,13 +18,34 @@ for (i = 0; i < (size * size); i++) {
 for (i = 0; i < squares.length; i++) {
     squares[i].id = `square${i+1}`;
     squares[i].className = 'square';
+    squares[i].style.flex = `1 0 ${(1/16) * 100}%`;
     if (((i +1) % size) !== 0){
-        sketcher.appendChild(squares[i]);
+        sketchArea.appendChild(squares[i]);
     } else {
-        sketcher.appendChild(squares[i]);
+        sketchArea.appendChild(squares[i]);
         const rowBreak = document.createElement('div');
-        rowBreak.className = "rowBreak";
-        sketcher.appendChild(rowBreak);
+        rowBreak.className = 'row-break';
+        sketchArea.appendChild(rowBreak);
     }
     
 }
+
+
+sketchArea.addEventListener('mouseover', (e) => {
+    const choice = e.target;
+    const choiceStyle = getComputedStyle(choice)
+    if (!choice.classList.contains('row-break') && choice.classList.contains('square')) {
+        if (!choice.classList.contains('has-color')){
+            const newColor = Math.floor(Math.random()*16777215).toString(16);
+            choice.style.backgroundColor = "#" + newColor;
+            choice.classList.toggle('has-color');
+        }
+        const currentOpacity = choiceStyle.opacity;
+        console.log(currentOpacity);
+        const newOpacity = ((parseFloat(currentOpacity) + .1));
+        console.log(newOpacity);
+    
+        choice.style.opacity = newOpacity;
+    }  
+    
+});
