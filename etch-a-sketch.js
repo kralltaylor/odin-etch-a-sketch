b9f04be7ba8c45b5ccbc6025d6c9d0b29bf009fa
Taylor Krall <168308491+kralltaylor@users.noreply.github.com>
2025-01-06 22:00:30 -0500

    Add ability to change grid size

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,32 +3,40 @@ container.style.padding = (container.width / 10);
 
 const sketchArea = document.querySelector('#sketch-area');
 
-const squareClass = document.querySelector('.square');
+const changeGrid = document.querySelector('#grid-size');
 
 let size = 16;
+let squares = [];
+
+createGrid();
 
 
-const squares = [];
-
-for (i = 0; i < (size * size); i++) {
-    squares.push(document.createElement('div'));
-}
 
 
-for (i = 0; i < squares.length; i++) {
-    squares[i].id = `square${i+1}`;
-    squares[i].className = 'square';
-    squares[i].style.flex = `1 0 ${(1/16) * 100}%`;
-    if (((i +1) % size) !== 0){
-        sketchArea.appendChild(squares[i]);
-    } else {
-        sketchArea.appendChild(squares[i]);
-        const rowBreak = document.createElement('div');
-        rowBreak.className = 'row-break';
-        sketchArea.appendChild(rowBreak);
+function createGrid () {
+    sketchArea.innerHTML = '';
+    squares = [];
+    for (i = 0; i < (size * size); i++) {
+        squares.push(document.createElement('div'));
     }
     
+    
+    for (i = 0; i < squares.length; i++) {
+        squares[i].id = `square${i+1}`;
+        squares[i].className = 'square';
+        squares[i].style.flex = `1 0 ${(1/size) * 100}%`;
+        if (((i +1) % size) !== 0){
+            sketchArea.appendChild(squares[i]);
+        } else {
+            sketchArea.appendChild(squares[i]);
+            const rowBreak = document.createElement('div');
+            rowBreak.className = 'row-break';
+            sketchArea.appendChild(rowBreak);
+        }
+        
+    }
 }
+
 
 
 sketchArea.addEventListener('mouseover', (e) => {
@@ -42,10 +50,34 @@ sketchArea.addEventListener('mouseover', (e) => {
         }
         const currentOpacity = choiceStyle.opacity;
         console.log(currentOpacity);
-        const newOpacity = ((parseFloat(currentOpacity) + .1));
+        const newOpacity = ((parseFloat(currentOpacity) + .2));
         console.log(newOpacity);
     
         choice.style.opacity = newOpacity;
     }  
     
 });
+
+changeGrid.addEventListener('click', (e) => {
+    changeGridSize();
+});
+
+function changeGridSize (){
+    let newsize = "";
+    do {
+        newSize = prompt("How many squares per side? (1 to 100)");
+        if (newSize == null){
+            return;
+        }
+        console.log(typeof(newSize));
+        newSize = parseInt(newSize);
+        console.log(newSize);
+        if (Number.isInteger(newSize) && newSize > 0 && newSize <= 100) {
+            size = newSize;
+            createGrid();
+        }
+        else {
+            alert("Invalid input. Please enter a number from 1 to 100.");
+        }
+    } while (!Number.isInteger(newSize) || (newSize <= 0) || (newSize > 100))
+}
